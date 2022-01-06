6295835431866d073664a6344fe4a2b28ad6ff37
bnasif25 <66963553+bnasif25@users.noreply.github.com>
2022-01-06 11:56:17 +0400

    Update index.js

--- index.js
+++ index.js
@@ -23,3 +23,11 @@ const returnDay = (num) => {
       }
       return (days[num]);
   }
+
+// Alternative Solution
+
+function returnDay(num) {
+    const days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    const day = days[num - 1];
+    return day ? day : null;
+}
